73d6569d9caaedc06ccb8a8f3da1987e23c9e78f
Ben Lardie <benlardie@gmail.com>
2019-04-08 16:26:28 -0400

    now there is a film component

---
+++ src/Film.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Film = ({film}) => {
+    return (
+    <article className='film-row'>
+    <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
+        <div className='film-summary'>
+            <h1>{film.title}</h1>
+            <p>{new Date(film.release_date).getFullYear()}</p>
+        </div>
+    </article>
+    )
+}
+
+export default Film;
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import TMDB from './TMDB';
+import Film from './Film';
 
 const FilmListing = () => {
 
     const filmList = TMDB.films.map((film) => {
         return (
-            <article className='film-row' key={film.id}>
-                <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
-                    <div className='film-summary'>
-                        <h1>{film.title}</h1>
-                        <p>{new Date(film.release_date).getFullYear()}</p>
-                    </div>
-            </article>
+            <Film film={film} key={film.id} />
         )
     })
 
