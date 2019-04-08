eaa81221b91799618e29a4bf003f1d08b26da4b5
Ben Lardie <benlardie@gmail.com>
2019-04-08 15:56:29 -0400

    date film was relased now displays

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,15 +3,20 @@ import TMDB from './TMDB';
 
 const FilmListing = () => {
 
-   
 
-   
+
 
     return (
         <div className='film-list'>
             <h1 className='section-title'>FILMS</h1>
+            <article className='film-row'>
             <img src={`https://image.tmdb.org/t/p/w780/${TMDB.films[0].poster_path}`} alt={TMDB.films[0].title} />
-        <h1>{TMDB.films[0].title}</h1>
+            <div className='film-summary'>
+            <h1>{TMDB.films[0].title}</h1>
+            <p>{new Date(TMDB.films[0].release_date).getFullYear()}</p>
+            </div>
+            </article>
+
         </div>
     )
 }
